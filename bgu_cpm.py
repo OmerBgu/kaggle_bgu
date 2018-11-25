ffa055cb3bfc7a902fbdae979b28ba2f5302b827
omer_an <anisfeld@post.bgu.ac.il>
2018-11-25 19:14:17 +0200

    fix NAN values in traget as 1 claassify and take cat code as X missign vlaues and predict with RF

--- bgu_cpm.py
+++ bgu_cpm.py
@@ -5,71 +5,47 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Imputer
 
-train_df = pd.read_csv(r'C:\Users\omer_an\Downloads\kaggle_bgu\all\saftey_efficay_myopiaTrain.csv')
-#train_df.dropna(inplace = True)
-train_df.fillna(method='bfill',inplace=True)
-print("{}{}".format(train_df.head(5), train_df.describe()))
-X_train = train_df.loc[:, train_df .columns != 'Class']
-y_train = train_df.loc[:, ~train_df.columns.isin(['Class'])]
 
-cleanup_nums = {"D_L_Sex":     {"F": 1, "M": 0},
-                "D_L_Eye": {"Right": 1, "Left": 0},
-                "T_L_Therapeutic_Cont._L.":     {"Yes": 1, "No": 0}}
-X_train.replace(cleanup_nums, inplace=True)
+def create_x(df, is_train):
+    # fill mising value with net valid sample
+    df.fillna(method='bfill', inplace=True)
+    # make train X and y , TODO : make to split for CV
+    if is_train:
+        X_train = df.drop(['Class'], axis=1)
+        y_train = df.Class
+    else:
+        X_train = df
+    # categorial to thire cat code
+    obj_df = X_train.select_dtypes(include=['object']).copy()
+    category = list(obj_df.columns)
+    for col in category:
+        X_train[col] = obj_df[col].astype('category')
+        X_train[col] = X_train[col].cat.codes
+    # fill NaN value with the mean of respective column
+    X_train.fillna(X_train.mean(), inplace=True)
+    if is_train:
+        y_train.fillna(1, inplace=True)  # just temporary put 1 calcification for missing values
+        return X_train, y_train
+    else:
+        return X_train, 0
 
-#print(train_df.isnull().sum())
-
-col_name = X_train.select_dtypes(include=['object']).copy().columns.values
-print(col_name)
-
-
-# values = X_train .values
-# imputer = Imputer()
-# cat_features = ['D_L_Sex', 'D_L_Eye', 'D_L_Dominant_Eye' 'Pre_L_Contact_Lens'
-#   'T_L_Laser_Type' 'T_L_Treatment_Type' 'T_L_Cust._Ablation' 'T_L_Micro'
-#   'T_L_Head' 'T_L_Therapeutic_Cont._L.' 'T_L_Epith._Rep.']
-# enc = LabelEncoder()
-# enc.fit(cat_features)
-# new_cat_features = enc.transform(cat_features)
-# print(new_cat_features)
-# new_cat_features = new_cat_features.reshape(-1, 1)
-# ohe = OneHotEncoder(sparse=False) #Easier to read
-# print(ohe.fit_transform(new_cat_features))
-#
-# enc = OneHotEncoder(categorical_features=cat_features)
-# enc.fit(X_train.values)
-#
-# label_encoder = LabelEncoder()
-# for col in col_name:
-#     print(col)
-#     X_train[col] = label_encoder.fit_transform(X_train[col])
-#
-#
-# print("after convert{}{}".format(X_train.dtypes, X_train.head()))
-#
-# enc = OneHotEncoder(handle_unknown='ignore')
-# enc.fit(X_train)
-# print(enc.categories_)
-#print("****not null : {}".format(X_train[X_train.isnull()]))
-
-# print("*********T_L_Epith._Rep. watch : {}".format(X_train['T_L_Epith._Rep.'].isnull().sum()))
-cat_df_onehot = X_train.copy()
-execpt_yes_no = ['T_L_Epith._Rep.', 'T_L_Therapeutic_Cont._L.']
-col_name_list = list(col_name)
-for col in execpt_yes_no :
-    cat_df_onehot= pd.get_dummies(cat_df_onehot, columns=[col], prefix = [col])
-    X_train= cat_df_onehot
-    col_name_list.remove(col)
-
-
-for l in col_name_list:
-    print("handle {}".format(l))
-    X_train[l] = X_train[l].astype('category')
-    X_train[l] = X_train[l].cat.codes
 
 rf = RandomForestRegressor(n_estimators=1000, random_state=42)
-print("--------------before fit")
 
+train_df = pd.read_csv(r'C:\Users\omer_an\Downloads\kaggle_bgu\all\saftey_efficay_myopiaTrain.csv')
+test_df = pd.read_csv(r'C:\Users\omer_an\Downloads\kaggle_bgu\all\saftey_efficay_myopiaTest.csv')
 
+# drop samples with no classification
+train_df.dropna(subset=['Class'], inplace= True)
+
+X_train, y_train = create_x(train_df, 1)
+
+X_test, dummy = create_x(test_df, 0)
+
+print("start fiting the model")
 rf.fit(X_train, y_train)
-print("--------------after fit")
+y_pred = rf.predict(X_test)
+print("after fit #############")
+print("feature_importances_ : {}".format(rf.feature_importances_))
+
+y_pred.to_csv(r'C:\Users\omer_an\Downloads\kaggle_bgu\all\submission.csv', sep=',', index = False)
